86668e1717c0dd69ed1d87dfa511b15a528b0f92
EtienneBis <165167093+EtienneBis@users.noreply.github.com>
2024-06-07 11:57:01 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -17,7 +17,8 @@ GITHUB_API_URL = "https://api.github.com/repos/OpenRSI/5MCSI_Metriques/commits"
 
 def extract_minutes(date_string):
         date_object = datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%SZ')
-        minutes = date_object.minute
+        #minutes = date_object.minute
+        return date_object.minute
 
 @app.route('/commits/')
 def NbCommits():
